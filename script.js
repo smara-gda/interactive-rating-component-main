a39cf180d6ab4e96d3627ece64ff95a138c52a98
Smaragda Kalogeropoulou <smaragda.kalogeropoulou@booking.com>
2022-11-04 16:59:55 +0100

    changes the rating to a form, adjusts css, small js adjustements

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-function toggleCard(result) {
-  const submitButton = document.querySelector('.card-button');
-  const sumbitCard = document.querySelector('.card-submit');
-  const thankYouCard = document.querySelector('.thankyou-card');
+let selectedCircle = document.querySelector(".card-rating__clicked");
 
-  submitButton.addEventListener('click', () => {
-    let finalResult = document.querySelector('.card-rate-selected .value');
-    const selectedCircle = document.querySelector('.card-rating__clicked');
+function toggleCard(result) {
+  const submitButton = document.querySelector(".card-button");
+  const sumbitCard = document.querySelector(".card-submit");
+  const thankYouCard = document.querySelector(".thankyou-card");
+
+  submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let finalResult = document.querySelector(".card-rate-selected .value");
+    selectedCircle = document.querySelector(".card-rating__clicked");
     if (!selectedCircle) {
-      alert('Please select a rating');
+      alert("Please select a rating");
     }
-    sumbitCard.style.display = 'none';
-    thankYouCard.style.display = 'flex';
+    sumbitCard.style.display = "none";
+    thankYouCard.style.display = "flex";
     finalResult.innerText = result;
   });
 }
 function selectedRating() {
-  const ratings = document.querySelectorAll('.card-rating__circle');
-  const selectedCircle = document.querySelector('.card-rating__clicked');
+  const ratings = document.querySelectorAll(".card-rating__circle");
+  selectedCircle = document.querySelector(".card-rating__clicked");
   ratings.forEach((item) => {
     item.addEventListener(
-      'click',
+      "click",
       (e) => {
         let targetedEl = e.target;
         if (selectedCircle) {
-          selectedCircle.classList.remove('card-rating__clicked');
+          selectedCircle.classList.remove("card-rating__clicked");
         }
-        targetedEl.classList.add('card-rating__clicked');
+        targetedEl.classList.add("card-rating__clicked");
         let ratingChosen = targetedEl.innerText;
         toggleCard(ratingChosen);
       },
-      false,
+      false
     );
   });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   selectedRating();
 });
